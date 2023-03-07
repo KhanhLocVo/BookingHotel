ce2a1d27ef54cfab32af7f4af51bce8d4bd4cfb5
MrTee <vokhanhloc1994@gmail.com>
2023-03-07 13:32:05 +0700

    complete the forgot password page functionality

--- bookinghotel/src/pages/ForgotPassword.jsx
+++ bookinghotel/src/pages/ForgotPassword.jsx
@@ -1,14 +1,28 @@
+import { async } from '@firebase/util';
 import React from 'react'
 import { useState } from 'react'
 import {BsEyeSlashFill, BsEyeFill } from 'react-icons/bs'
 import { Link } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { toast } from 'react-toastify';
+import {getAuth, sendPasswordResetEmail} from 'firebase/auth';
   
 export default function ForgotPassword() {
   const [email, setEmail] = useState("");
   
   function onChange(e){
    setEmail(e.target.value);
+  }
+
+  async function onSubmit(e){
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Email was sent");
+    } catch (error) {
+      toast.error("Could not send reset password");
+    }
   }
   return (
     <section>
@@ -19,7 +33,7 @@ export default function ForgotPassword() {
           className="w-full rounded-2xl" />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form  action="">
+          <form  onSubmit={onSubmit}>
             <input type="email" id="email" value={email} onChange={onChange} placeholder="email address"
              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out" 
             />
@@ -33,16 +47,17 @@ export default function ForgotPassword() {
                 className="text-blue-600 hover:text-blue-700 transition duration-200 ease-in-out ml-1">Sign in instead</Link>
               </p>
             </div>
-            
-          </form>
-          <button className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
-           type="submit">Send reset password</button>
-          <div className="flex items-center my-4
+            <button className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+            type="submit">Send reset password</button>
+            <div className="flex items-center my-4
             before:border-t before:flex-1 before:border-gray-300
             after:border-t after:flex-1 after:border-gray-300">
             <p className="text-center font-semibold mx-4">OR</p>
           </div>
           <OAuth/>
+            
+          </form>
+          
 
           
         </div>
