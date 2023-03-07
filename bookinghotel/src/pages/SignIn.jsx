fd32721b508e9f377543759265a189202e97973b
MrTee <vokhanhloc1994@gmail.com>
2023-03-07 13:18:58 +0700

    complete the sign in functionality

--- bookinghotel/src/pages/SignIn.jsx
+++ bookinghotel/src/pages/SignIn.jsx
@@ -3,20 +3,40 @@ import { useState } from 'react'
 import {BsEyeSlashFill, BsEyeFill } from 'react-icons/bs'
 import { Link } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import {signInWithEmailAndPassword, Auth, getAuth} from 'firebase/auth';
+import {useNavigate} from 'react-router-dom';
+import {toast} from 'react-toastify';
+import { async } from '@firebase/util';
   
 export default function SignIn() {
+  
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
     email : "",
     password : "",
   });
   const {email, password} = formData;
+  const navigate = useNavigate();
   function onChange(e){
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value,
     }));
   }
+
+  async function onSubmit(e){
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredentials = await signInWithEmailAndPassword(auth, email, password);
+      if(userCredentials.user){
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error("Bad user credentials");
+    }
+  }
+
   return (
     <section>
       <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
@@ -26,7 +46,7 @@ export default function SignIn() {
           className="w-full rounded-2xl" />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form  action="">
+          <form onSubmit={onSubmit}>
             <input type="email" id="email" value={email} onChange={onChange} placeholder="email address"
              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out" 
             />
@@ -54,16 +74,17 @@ export default function SignIn() {
                 className="text-blue-600 hover:text-blue-700 transition duration-200 ease-in-out ml-1">Forgot Password</Link>
               </p>
             </div>
-            
-          </form>
-          <button className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
-           type="submit">Sign In</button>
-          <div className="flex items-center my-4
+            <button className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+            type="submit">Sign In</button>
+            <div className="flex items-center my-4
             before:border-t before:flex-1 before:border-gray-300
             after:border-t after:flex-1 after:border-gray-300">
             <p className="text-center font-semibold mx-4">OR</p>
           </div>
           <OAuth/>
+            
+          </form>
+          
 
           
         </div>
