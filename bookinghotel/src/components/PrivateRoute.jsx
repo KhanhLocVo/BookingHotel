d9e2a21a516729ffabfe90a18b07d9117ff5d8a9
MrTee <vokhanhloc1994@gmail.com>
2023-03-09 18:54:00 +0700

    create the spinner component

--- bookinghotel/src/components/PrivateRoute.jsx
+++ bookinghotel/src/components/PrivateRoute.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Outlet, Navigate } from 'react-router-dom';
 import {useAuthStatus} from '../hooks/useAuthStatus';
+import Spinner from "./Spinner.jsx"
 
 export default function PrivateRoute() {
     const {loggedIn, checkingStatus} = useAuthStatus();
     if(checkingStatus){
-        return <img src="./images/giphy.gif" alt=""
-        className="pt-20 md:w-[30%] lg:w-[20%] mb-12 md:mb-6 md:m-auto rounded-full "
-        />
+        return <Spinner/>;
     }
   return  loggedIn ? <Outlet/> : <Navigate to="/sign-in"/>
 }
